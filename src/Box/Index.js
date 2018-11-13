de61e4dbff1ed602c614e2236b68ef0269c0db34
deam <deamme@users.noreply.github.com>
2018-11-13 22:44:15 +0100

    Added Box component

---
+++ src/Box/Index.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import {
+  space,
+  display,
+  alignItems,
+  flexDirection,
+  justifyContent,
+  textAlign,
+  flexWrap,
+} from 'styled-system'
+
+export default styled.div`
+  ${space}
+  ${display}
+  ${alignItems}
+  ${flexDirection}
+  ${justifyContent}
+  ${textAlign}
+  ${flexWrap}
+`
